95a798316f471d0bb0175d1c6d48fa0125a98cc6
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2024-11-01 18:49:43 +0530

    Operators
    Combined assignment operator

---
+++ Combined_assignment_operators.js
@@ -0,0 +1,28 @@
+let num = 5
+
+num +=10
+console.log(num);
+
+num *=5
+console.log(num);
+
+num /=5
+console.log(num);
+
+num **=5
+console.log(num);
+
+num %=5
+console.log(num);
+
+num++
+console.log(num);
+
+++num
+console.log(num);
+
+--num
+console.log(num);
+
+num--
+console.log(num);
