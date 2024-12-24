d003cdeb632068217887ab2fdb1e347a0fc97eee
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2024-12-24 09:40:55 +0530

    Functions
    Functions

---
+++ functions_pro.js
@@ -0,0 +1,8 @@
+function introduce(username, professional){
+    console.log("Hello,");
+    console.log(`I am ${username}.`);
+    console.log(`I am a ${professional}.`);
+}
+
+introduce("ram", "developer")
+introduce("hari", "Engineer")
