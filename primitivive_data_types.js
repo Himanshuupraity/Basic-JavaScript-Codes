3065bdac13a2b55526b961d5b36d5471577f58d7
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2024-12-25 17:26:07 +0530

    primitive data types

---
+++ primitivive_data_types.js
@@ -0,0 +1,9 @@
+console.log(typeof(5.7)); // number
+console.log(typeof("This is string")); // string
+console.log(typeof(true)); // boolean
+console.log((+true)); // 1
+console.log(typeof(false)); // boolean
+console.log((+false)); // 0
+console.log(typeof(undefined)); // undefined
+console.log(typeof(null));  // object
+console.log(typeof(5243534636756757667567567325)); // number, big int
