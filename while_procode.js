4aa5e5ce0ef0119a48b608da92755f18bb7291a1
Himanshu Upraity <144529158+Himanshuupraity@users.noreply.github.com>
2024-12-31 10:04:27 +0530

    while procode

---
+++ while_procode.js
@@ -0,0 +1,11 @@
+console.log('program Started')
+
+const a=["apple", "ball","cat"]
+
+let i=0
+while(i<a.length){
+    console.log(`${i+1}.${a[i]} ${"Animal"}`);
+    i++
+}
+
+console.log("Program ended")
